5fcffa04695864528724fae27273779b593802ac
Paulo Sousa <53922215+pfrsousa2010@users.noreply.github.com>
2025-06-20 16:21:55 -0300

    Remove Vue plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -14,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    vue(),
     react(),
     mode === "development" &&
       componentTagger(),
